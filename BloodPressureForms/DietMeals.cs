b16d8a4d785ff76d81e67740283f679ead255a14
LamisHasan <lamishassan17@yahoo.com>
2019-12-20 19:39:29 +0200

    Finsished ViewDietMeal Form

--- BloodPressureForms/DietMeals.cs
+++ BloodPressureForms/DietMeals.cs
@@ -22,7 +22,43 @@ namespace BloodPressureForms
             CRUDService.CRUDClient crud = new CRUDService.CRUDClient();
             List<string> Meals = new List<string>();
             Meals = crud.viewPersonDiet(Login.Person_ID).ToList();
-            label1.Text = Meals[0];
+            DietName_value.Text = Meals[0];
+            DietType_value.Text = Meals[1];
+            MealBreakfast_value.Text = Meals[2];
+            MealBreakfast.Text = Meals[3];
+            MealLunch_value.Text = Meals[4];
+            MealLuch.Text = Meals[5];
+            MealDinner_value.Text = Meals[6];
+            MealDinner.Text = Meals[7];
+
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            UserDetails userDetails = new UserDetails();
+            userDetails.Show();
+            this.Hide();
         }
     }
 }
